517b613c57bac789902ce528228877b855060621
JoshMarte <joshuelm@gmail.com>
2021-10-14 11:55:23 -0400

    completed display-info event

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 fetch("https://ghibliapi.herokuapp.com/films").then((res) => {
   res.json().then((data) => {
-    for (let item of data) {
+    for (let i = 0; i < data.length; i++) {
       let option = document.createElement("option");
-      option.value = item.title;
-      option.textContent = item.title;
+      option.value = i;
+      option.textContent = data[i].title;
 
-      if (item.title === "Castle in the Sky") {
+      if (data[i].title === "Castle in the Sky") {
         option.setAttribute("selected", "selected");
       }
       document.querySelector("#moviesList").append(option);
     }
+
+    document
+      .querySelector("#dropDown select")
+      .addEventListener("change", (event) => {
+        document.querySelector("#display-info h3").textContent =
+          data[event.target.value].title;
+        document.querySelector("#display-info #year").textContent =
+          data[event.target.value].release_date;
+        document.querySelector("#display-info #desc").textContent =
+          data[event.target.value].description;
+      });
   });
 });
